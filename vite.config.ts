68b90e33ba29c5bb9e5a1ae2f02bdcbe6a577cd0
Dominik Tichy <tichydominik451@gmail.com>
2024-12-15 22:19:09 +0100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { rename } from 'fs/promises';
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  plugins: [
+    react(),
+    // rename output html file so that it works with surge
+    {
+      name: 'rename-html',
+      writeBundle: async () => {
+        const distDir = './dist/';
+        try {
+          await rename(`${distDir}index.html`, `${distDir}200.html`);
+        } catch (err) {
+          console.error('Failed to rename index.html to 200.html:', err);
+        }
+      },
+    },
+  ],
 });
